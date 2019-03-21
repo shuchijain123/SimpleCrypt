345591df758fb70a94a75442b434c2a3cd06bbb0
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-03-21 13:38:13 -0400

    adding simple crypt

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -2,31 +2,100 @@ import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class ROT13 {
+   Character cf=' ';
+   Character cs =' ';
+
 
     ROT13(Character cs, Character cf) {
+        this.cs=cs;
+        this.cf=cf;
     }
 
     ROT13() {
+
+        this.cs=' ';
+        this.cf =' ';
+
     }
 
 
-    public String crypt(String text) throws UnsupportedOperationException {
+    public String crypt(String text)  {
 
-        return "";
+        return encrypt(text);
     }
 
     public String encrypt(String text) {
-        return text;
+        String result = "";
+
+        char[] chArr = text.toLowerCase().toCharArray();
+
+        for (char ch : chArr) {
+            if ((ch == ' ') || (ch == '?') || (ch == '!')) {
+
+                result += ch;
+            } else
+
+                result += getChar(ch).charValue();
+
+
+
+        }
+        return result.substring(0, 1).toUpperCase() + result.substring(1);
     }
 
+
+
+
     public String decrypt(String text) {
-        return text;
+        return encrypt(text);
     }
 
     public static String rotate(String s, Character c) {
 
-        return "";
+
+       if ((s.charAt(0) =='A') && (c == 'A')) {
+
+           return s;
+       }
+           StringBuilder sb = new StringBuilder();
+           for (int i = (s.length() / 2); i < s.length(); i++) {
+               sb.append(s.charAt(i));
+           }
+           for (int j = 0; j < s.length() / 2; j++) {
+               sb.append(s.charAt(j));
+           }
+           return sb.toString();
+
+
+
     }
 
+    public Character getChar(Character ch) {
+
+        Map<Character, Character> mymap = new HashMap();
+
+        for (int i = 97; i < 122; i++) {
+
+            if (i < 110) {
+
+                mymap.put((char) i, (char) (i + 13));
+            } else {
+
+                mymap.put((char) i, (char) (i - 13));
+
+
+            }
+
+
+        }
+        return mymap.get(ch);
+
+
+    }
 }
